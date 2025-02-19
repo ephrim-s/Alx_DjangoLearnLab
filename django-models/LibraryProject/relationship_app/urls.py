f546f93612167e9c40535da86d5a84020be9ceb6
ephrim-s <ephrims5@gmail.com>
2025-02-19 21:07:26 +0000

    updated

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.all_books, name="books"),
+    path('booklist/', views.BookListView.as_view(), name='library'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import (Author, Book, Library, Librarian)
+from django.views.generic import ListView
 
-# Create your views here.
+def all_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {"books": books})
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'library_detail.html'
+    context_object_name = "library"
