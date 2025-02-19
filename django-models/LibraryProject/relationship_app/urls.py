4dcb32a4be7d137398b80cb9f6c3fc7610f17aa3
ephrim-s <ephrims5@gmail.com>
2025-02-19 21:24:57 +0000

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', views.all_books, name="books"),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,9 +6,9 @@ from django.views.generic.detail import DetailView
 
 def all_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {"books": books})
+    return render(request, 'list_books.html', {"books": books})
 
 class BookListView(ListView):
     model = Book
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = "library"
