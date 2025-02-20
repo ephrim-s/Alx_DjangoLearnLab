9f6c97fa3d11b5ddc9102b14ad7b11b43307ac15
ephrim-s <ephrims5@gmail.com>
2025-02-20 10:04:48 +0000

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # from django.http import HttpResponse
 # from django.template import loader
 from .models import Library, Book
@@ -25,3 +27,29 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books"] = self.object.book_set.all()  # Assuming a ForeignKey from Book to Library
         return context
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form':form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
