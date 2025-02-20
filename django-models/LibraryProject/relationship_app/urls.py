19c6000a4e869cc0db1f9f1081d44e9b57855de4
ephrim-s <ephrims5@gmail.com>
2025-02-20 16:54:08 +0000

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, login_view, logout_view
+from .views import list_books, LibraryDetailView, LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('books/', list_books, name="books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     path("register/", views.register, name='register'),
-    path("login/", login_view, name='login'),
-    path("logout/", logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # from django.http import HttpResponse
 # from django.template import loader
@@ -39,17 +40,8 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form':form})
 
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('books')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form':form})
+class LoginView(LoginView):
+    template_name = 'relationship_app/login.html'
 
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+class LogoutView(LogoutView):
+    template_name = 'authentication/logout.html'
