b6e1c897312b72d9e036f3fe59cab6439eaf1f6f
ephrim-s <ephrims5@gmail.com>
2025-02-22 07:57:46 +0000

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView, Admin
+from .views import list_books, LibraryDetailView, LoginView, LogoutView, Admin, add_book, edit_book, delete_book
 from . import views
 # from .admin_view import admin_dashboard
 # from .librarian_view import librarian_dashboard
@@ -11,6 +11,9 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('add/', add_book, name='add_book'),
+    path('edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete/<int:boo_id>/', delete_book, name='delete_book'),
     path('admin/', Admin, name='admin_dashboard'),
     # path('librarian/', librarian_dashboard, name='librarian_dashboard'),
     # path('member/', member_dashboard, name='member_dashboard'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Library, Book
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from .forms import BookForm
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method =="POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'delete_book.html', {'book': book})
 
 
 def list_books(request):
