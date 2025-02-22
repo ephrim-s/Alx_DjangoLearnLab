622ddf9aebaf2550475017d69a6687a81f701cab
ephrim-s <ephrims5@gmail.com>
2025-02-22 03:29:38 +0000

    updated

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-def Admin(user):
+def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-# @login_required
-@user_passes_test(Admin)
+@login_required
+@user_passes_test(is_admin)
 def admin_dashboard(request):
     return render(request, 'admin_dashboard.html')
-
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 def list_books(request):
@@ -38,4 +39,28 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_dashboard(request):
+    return render(request, 'Librarian_dashboard.html')
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_member)
+def member_dashboard(request):
+    return render(request, 'member_dashboard.html')
 
