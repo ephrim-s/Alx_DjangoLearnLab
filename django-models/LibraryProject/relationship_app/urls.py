575c2bd931745056949ef8736bfb882487d2532e
ephrim-s <ephrims5@gmail.com>
2025-02-22 07:31:47 +0000

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView
+from .views import list_books, LibraryDetailView, LoginView, LogoutView, Admin
 from . import views
-from .admin_view import admin_dashboard
-from .librarian_view import librarian_dashboard
-from .member_view import member_dashboard
+# from .admin_view import admin_dashboard
+# from .librarian_view import librarian_dashboard
+# from .member_view import member_dashboard
 
 urlpatterns = [
     path('books/', list_books, name="books"),
@@ -11,7 +11,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin/', admin_dashboard, name='admin_dashboard'),
-    path('librarian/', librarian_dashboard, name='librarian_dashboard'),
-    path('member/', member_dashboard, name='member_dashboard'),
+    path('admin/', Admin, name='admin_dashboard'),
+    # path('librarian/', librarian_dashboard, name='librarian_dashboard'),
+    # path('member/', member_dashboard, name='member_dashboard'),
 ]
