79e247263f52ba236c9f00e3892cb82b74544e77
ephrim-s <ephrims5@gmail.com>
2025-03-16 06:15:26 +0000

    added generics CRUD

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.BookListView.as_view(), name="book-list"),
+    path("books/detail/<int:pk>/", views.BookDetailView.as_view(), name="book-details"),
+    path("books/create/", views.BookCreateView.as_view(), name="create-book"),
+    path("books/update/<int:pk>/", views.BookUpdateView.as_view(), name="update-book"),
+    path("book/delete/<int:pk>/", views.BookDeleteView.as_view(), name="delete-book"),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_class = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_class = [IsAuthenticatedOrReadOnly]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_class = [IsAuthenticatedOrReadOnly]
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_class = [IsAuthenticatedOrReadOnly]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_class = [IsAuthenticatedOrReadOnly]
