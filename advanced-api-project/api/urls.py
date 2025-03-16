2e686196fc5cdfbb8fe659bc4bceecf822191625
ephrim-s <ephrims5@gmail.com>
2025-03-16 06:17:30 +0000

    updated

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("books/detail/<int:pk>/", views.BookDetailView.as_view(), name="book-details"),
     path("books/create/", views.BookCreateView.as_view(), name="create-book"),
     path("books/update/<int:pk>/", views.BookUpdateView.as_view(), name="update-book"),
-    path("book/delete/", views.BookDeleteView.as_view(), name="delete-book"),
+    path("books/delete/<int:pk>/", views.BookDeleteView.as_view(), name="delete-book"),
 ]
