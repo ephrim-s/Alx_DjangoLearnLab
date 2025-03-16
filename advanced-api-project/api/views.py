0a1dbe0ddc90ac3f5a99f48212119ba2a37b2fec
ephrim-s <ephrims5@gmail.com>
2025-03-16 06:30:48 +0000

    added filter and serach

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+from django_filters import FilterSet, CharFilter, NumberFilter
 from rest_framework import generics
+from rest_framework import OderingFilter
+from rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import BookSerializer
 from .models import Book
+
+class BookFilter(FilterSet):
+    title = CharFilter(lookup_expr='icontains', label='Filter by title')
+    author = CharFilter(lookup_expr='icontains', label='Filter by author')
+    publication_year = NumberFilter(field_name='published_date__year', lookup_expr='exact', label='Filter by publication year')
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
 
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_class = [IsAuthenticatedOrReadOnly]
+    filter_backends = (DjangoFilterBackend, OderingFilter)
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'published_date']
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -29,3 +46,6 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_class = [IsAuthenticatedOrReadOnly]
+
+
+
