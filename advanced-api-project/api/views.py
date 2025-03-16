1233f094442511d63a25f241bb0c972ead20f866
ephrim-s <ephrims5@gmail.com>
2025-03-16 06:46:26 +0000

    updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django_filters import FilterSet, CharFilter, NumberFilter
 from django_filters import rest_framework
 from rest_framework import generics
-from rest_framework.filters import OrderingFilter
+from rest_framework import filters.OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import BookSerializer
