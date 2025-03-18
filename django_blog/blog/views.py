55305b8b24c6b02214b7253b7323d341fa870e3f
ephrim-s <ephrims5@gmail.com>
2025-03-18 06:33:35 +0000

    updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form':form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user = request.user
+        user.email = request.POST.get('email', user.email)
+        user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html', {'user': request.user})
