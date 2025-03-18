1ebcd1f99367d254bf0c3ccebb677f207d6a4b7b
ephrim-s <ephrims5@gmail.com>
2025-03-18 05:31:43 +0000

    updated

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    pushblished_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
