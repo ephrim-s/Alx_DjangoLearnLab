b953e98b979736c493ff426d76297b81f21e51ea
ephrim-s <ephrims5@gmail.com>
2025-03-30 20:06:23 +0000

    updated

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets, permissions, filters
+from .models import Post, Comment
+from .serializers import *
+from rest_framework.pagination import PageNumberPagination
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'posts',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -108,6 +109,8 @@ REST_FRAMEWORK = {
     'DEFAULT_ATHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
