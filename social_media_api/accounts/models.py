1f973038308e39a2b99e7bc7495d74811aaa35e5
ephrim-s <ephrims5@gmail.com>
2025-03-30 20:43:47 +0000

    updated

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True,  null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='followings')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followingss')
+    
 
     def __str__(self):
         return self.username
