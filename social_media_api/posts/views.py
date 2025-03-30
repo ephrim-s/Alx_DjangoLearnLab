7b7846fe93105506138b24b4a43c45425374489b
ephrim-s <ephrims5@gmail.com>
2025-03-30 20:55:39 +0000

    updated

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -28,6 +28,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 @permission_classes([permissions.IsAuthenticated])
 def user_feed(request):
     following_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=following_users)
+    posts = Post.objects.filter(author__in=following_users.order_by('-created_at'))
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
